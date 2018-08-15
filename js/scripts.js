6fb6f612b14bd92f85edeb79edc908991a09f690
Victoria Oh and Vera Weikel <vera@>
2018-08-15 13:46:51 -0700

    It will show test to have input populate alert function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
+
+  var vowels = ["a", "e", "i", "o", "u", "A", "E","I","O","U"];
+  var consanants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x"];
+
+// ,"y","z","B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W","X", "Y","Z"
+
+  // if (inputTextVerbiage != vowels) || (inputTextVerbiage != consonants){
+  //   return inputTextVerbiage;
+  // };
+
+
+
+
+
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
 
   var inputTextVerbiage = $("input#inputTextVerbiage").val();
-  var outputTextTranslation = translation(inputTextVerbiage);
+  // var outputTextTranslation = translation(inputTextVerbiage);
 
-  $("#outputTextTranslation").text(outputTextTranslation);
-
-
+  alert(inputTextVerbiage);
 
   });
+
 });
