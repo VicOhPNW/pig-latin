2b3dcb64319dfa14eb27ac7ec9c85bd1cc650783
Victoria Oh and Vera Weikel <victoria@>
2018-08-15 16:46:22 -0700

    It will show single vowels identified as vowels and returns ay attached to the vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,47 @@ var vowels = ["a", "e", "i", "o", "u", "A", "E","I","O","U"];
 // ,"y","z","B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W","X", "Y","Z"
 
 
-//checks if starting character is vowel or consanant - boolean
-//   function checkStartingLetter(string) {
-//     if (vowels.includes(string.charAt(0))) {
-//       return "vowel"
-//   } else {
-//     return "consonant"
-//   }
-// }
-function checkStartingLetter(string) {
-  if (vowels.includes(string.charAt(0))) {
-    return string;
-
-  }
-}
+// checks if starting character is vowel or consanant - boolean
+  function checkStartingLetter(string) {
+    if (vowels.includes(string.charAt(0))) {
+      return "vowel"
   } else {
     return "consonant"
   }
 }
 
+function outputVowelWord (string){
+  if (vowels.includes(string.charAt(0))) {
+    var inputText = string + "ay";
+    return inputText;
+  };
+};
 
 
-  // if (inputText != vowels) || (inputText != consonants){
-  //   return inputText;
+
+
+
+
+
+// function checkStartingLetter(string) {
+//   if (vowels.includes(string.charAt(0))) {
+//     return string;
+//     // console.log(inputText.join('ay'));
+//
+//     returnWord += "ay";
+//   }
+//
+// }
+//    else {
+//     return "consonant"
+//   }
+// };
+
+
+
+  // if (input#inputText.includes(vowels)) || (inputText != consonants){
+  //   return inputText2 = (inputText+'ay');
   // };
-
 
 
 $(document).ready(function(){
@@ -38,10 +54,10 @@ $(document).ready(function(){
     event.preventDefault();
 
   var inputText = $("input#inputText").val();
-  // var outputText = translation(inputText);
+  var outputText = outputVowelWord(inputText);
 
-  $("#outputText").text(inputText);
+  $("#outputText").text(outputText);
 
-  })
+  });
 
-})
+});
